79c268da4743ac7a7146b14db3234cd9e7d39084
Ferreyra Maximiliano Gaston <mgaston.ferreyra@gmail.com>
2023-11-10 13:42:16 -0300

    Primera version de Registro de Usuario

---
+++ negocio/Usuarios.cs
@@ -0,0 +1,86 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+
+namespace Negocio
+
+{
+    public class Usuarios
+    {
+
+        public static List<Usuario> Listar()
+        {
+            AccesoDatos acceso = new AccesoDatos();
+            List<Usuario> articulos = new List<Usuario>();
+
+            var lector = acceso.Leer("SELECT * FROM Usuarios ORDER BY Id");
+
+            while (lector.Read())
+            {
+                Usuario aux = new Usuario
+                {
+                    Id = (int)lector["Id"],
+                    Apellidos = (string)lector["Apellidos"],
+                    Nombres = (string)lector["Nombre"],
+                    DNI = (string)lector["DNI"],
+                    Email = (string)lector["Email"],
+                    FechaNacimiento = (DateTime)lector["FechaNacimiento"],
+                    Password = (string)lector["Password"],
+                    Telefono = (string)lector["Telefono"],
+                    IdTipoUsuario = (int)lector["IdTipoUsuario"],
+                    IdDomicilio = (int)lector["IdDomicilio"],
+                };
+
+                articulos.Add(aux);
+            }
+
+            return articulos;
+        }
+
+        public static Usuario Leer(string mail, string password)
+        {
+            AccesoDatos acceso = new AccesoDatos();
+
+            var lector = acceso.Leer("SELECT * FROM Usuario WHERE Email = '" + mail + "' AND Password = '" + password + "'");
+
+            if (!lector.Read()) return null;
+
+            Usuario usr = new Usuario
+            {
+                Id = (int)lector["Id"],
+                Apellidos = (string)lector["Apellidos"],
+                Nombres = (string)lector["Nombre"],
+                DNI = (string)lector["DNI"],
+                Email = (string)lector["Email"],
+                FechaNacimiento = (DateTime)lector["FechaNacimiento"],
+                Password = (string)lector["Password"],
+                Telefono = (string)lector["Telefono"],
+                IdTipoUsuario = (int)lector["IdTipoUsuario"],
+                IdDomicilio = (int)lector["IdDomicilio"],
+            };
+
+            return usr;
+        }
+
+        public static bool Existe(string mail)
+        {
+            AccesoDatos acceso = new AccesoDatos();
+            var lector = acceso.Leer("SELECT Id FROM Usuario WHERE Email = '" + mail + "'");
+
+            return lector.Read();
+        }
+
+        public static bool Grabar(Usuario usr)
+        {
+            AccesoDatos acceso = new AccesoDatos();
+
+            List<string> query = new List<string> {
+                string.Format("INSERT INTO Usuario (Apellidos, Nombres, Email, Password, DNI, FechaNacimiento, Telefono, IdTipoUsuario, IdDomicilio, Activo) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}')",
+                usr.Apellidos, usr.Nombres, usr.Email, usr.Password, usr.DNI, usr.FechaNacimiento.ToString("yyyy-MM-dd"), usr.Telefono, usr.IdTipoUsuario, usr.IdDomicilio, "1")
+            };
+
+            return acceso.Ejecutar(query) > 0;
+        }
+
+    }
+}
--- tp-final-equipo-8/InicioSesion.aspx
+++ tp-final-equipo-8/InicioSesion.aspx
@@ -7,7 +7,7 @@
   class="relative"
 >
   <div>
-
+      
     <main
       class="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
     >
@@ -19,6 +19,22 @@
     
 
         <form action="#" class="mt-8 grid grid-cols-6 gap-6">
+            
+          <% if (Session["Msg_error"] != null)
+              { %>
+              <div class="bg-red-200 p-10 absolute top-2 right-2">
+                  <%= Session["Msg_error"] %>
+                  <% Session["Msg_error"] = null; %>
+              </div>
+          <%} %>
+          <% if (Session["Msg_ok"] != null)
+              { %>
+              <div class="bg-green-200 p-10 absolute top-2 right-2">
+                  <%= Session["Msg_ok"] %>
+                  <% Session["Msg_ok"] = null; %>
+              </div>
+          <%} %>
+
           <div class="col-span-6 space-y-2">
             <label for="UserEmail" class="bg-slate-800 relative block overflow-hidden rounded-md border border-slate-900 px-3 pt-3 shadow-sm focus-within:border-slate-600 focus-within:ring-1 focus-within:ring-slate-600">
                 <input type="email" id="UserEmail" placeholder="Email" class="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"/>
@@ -45,7 +61,7 @@
 
             <p class="mt-4 text-sm text-slate-300 sm:mt-0">
               No tenes una cuenta? 
-              <a href="#" class="text-slate-400 underline">Registrate</a>.
+              <a href="/Registro.aspx" class="text-slate-400 underline">Registrate</a>.
             </p>
           </div>
         </form>
